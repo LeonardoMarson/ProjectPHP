e7f7f019950b9d558435efb248c3b57adcb2fe1a
LeonardoMarson <leonardomateusmarson@gmail.com>
2022-10-18 22:08:20 -0300

    criacao pagina index

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrar</title>
+</head>
+<body>
+    <div>
+        <h1>Title</h1>
+    </div>
+    <form action="">
+        <div>
+            <label for="">Usuário</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Senha</label>
+            <input type="password">
+        </div>
+        <div>
+            <a href="">Esqueceu sua senha?</a>
+            <button type="submit">Enviar</button>
+        </div>   
+        <div>
+            <p>Não tem uma conta?</p>
+            <a href="">Inscrever-se no</a>
+        </div>
+    </form>
+</body>
+</html>
