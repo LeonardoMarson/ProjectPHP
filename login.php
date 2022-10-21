2f8c4662bde7a4ae524958c5588aab91754c837f
Everton Zardo <evezardo15@gmail.com>
2022-10-21 15:55:03 -0300

    Adicionadas variaveis de sessao Variaveis de sessao baseadas no retorno dos dados da Google API, por enquanto utilizadas para testes

--- login.php
+++ login.php
@@ -1,23 +1,29 @@
 <?php
     require_once 'vendor/autoload.php';
 
+    session_start();
+
     $googleClient = new Google\Client();
     $googleClient->setAuthConfig('client_secret.json');
     $googleClient->addScope('https://www.googleapis.com/auth/userinfo.profile');
     $googleClient->addScope('https://www.googleapis.com/auth/userinfo.email');
 
     if(isset($_POST['credential'])){
-        echo "ENTERED!";
+        $id_token = $_POST['credential'];
+        $payload = $googleClient->verifyIdToken($id_token);
+
+        if (isset($payload['email'])) {
+            $name = $payload['name'];
+            $email = $payload['email'];
+
+            $_SESSION['userName'] = $name;
+            $_SESSION['email'] = $email;
+        }
+
+        print_r($_SESSION);
     }
     else {
+        echo "Credential or user not found!";
         header('Refresh: 2; URL=index.php');
-    }
-
-    $id_token = $_POST['credential'];
-    $payload = $googleClient->verifyIdToken($id_token);
-    if (isset($payload['email'])) {
-        echo '<pre>';
-        print_r($payload);
-        echo '</pre>';
     }
 ?>
